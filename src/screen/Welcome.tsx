17135f1913c92894a37176fe894a053f6d1adceb
Upeksha1214 <upekshasachintha@gmail.com>
2023-09-14 08:18:00 -0700

    welcome is done

--- src/screen/Welcome.tsx
+++ src/screen/Welcome.tsx
@@ -1,5 +1,6 @@
-import { Button, ScrollView, StyleSheet, Text, View } from 'react-native'
+import { Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+
 
 // navigation
 import { NativeStackScreenProps } from "@react-navigation/native-stack"
@@ -11,12 +12,13 @@ type WelcomeProps = NativeStackScreenProps<RootStackParamList, 'Welcome'>
 export default function Welcome({navigation}:WelcomeProps) {
   return (
     <ScrollView style={styles.container}>
-
-      <View style={styles.container}>
-        <Text style={styles.smallText}>Home Screen</Text>
-      </View>
       <View>
-        
+      <TouchableOpacity
+          onPress={() => navigation.navigate("Profile")}
+        >
+          <Image source={require('../assert/icon/setting.png')} 
+          style={styles.image}/>
+        </TouchableOpacity>
       </View>
     </ScrollView>
   )
@@ -30,5 +32,21 @@ const styles = StyleSheet.create({
   },
   smallText: {
     color: "#000000"
-  }
+  },
+  button: {
+    flex: 1,
+    marginTop: 50,
+    backgroundColor: "#FFD482",
+    width: "92%",
+    height: 48,
+    left: 14,
+    borderRadius: 10,
+    justifyContent: "center",
+
+  },
+  image: {
+    width: 50, 
+    height: 50,
+    left: 350
+  },
 })
